c90fef0c336a09261c99024dc278d762867bd773
Rajath-B <rajath@techis.io>
2022-09-22 00:12:06 +0530

    Updated Files

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'cloudinary',
+    'corsheader',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
